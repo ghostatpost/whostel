9340426e5202c24bd8ca5cf45d0f4953e0a152b4
ghostatpost <ghostatpost@gmail.com>
2020-05-09 13:31:22 +0530

    2 files modified

--- src/app/mark/mark.component.ts
+++ src/app/mark/mark.component.ts
@@ -6,11 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mark.component.css']
 })
 export class MarkComponent  {
-  nam=[{name:'sr'
-  },{name:'kaushalya',
-  image:<img src="{{}}">},
-  {name:'ar'},
-  {name: 'gr'},{name:'vr'}
+  nam=[{name:'sr',
+  rate:3.5,
+  image: "assets/images/1.jpg"
+  },
+  {name:'kaushalya',
+  rate:3.5,
+  image: "assets/images/2.jpg"},
+  {name:'ar',
+  rate:3.5,
+  image: "assets/images/3.jpg"},
+  {name: 'gr',
+  rate:3.5,
+  image: "assets/images/4.jpg"},
+  {name:'vr',
+  rate:3.5,
+  image: "assets/images/5.jpg"}
   ];
 images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 }
